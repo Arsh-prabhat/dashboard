2bc332ad368ae7ae893016e2eb3b281aa294c245
Arsh <prabhatarsh1974@gmail.com>
2025-12-18 13:06:19 +0530

    db_services changed

--- services/db_service.py
+++ services/db_service.py
@@ -1,20 +1,16 @@
 import pyodbc
-import os
 
 def get_db_connection():
-    server = os.getenv("DB_SERVER")
-    database = os.getenv("DB_NAME")
-    username = os.getenv("DB_USER")
-    password = os.getenv("DB_PASSWORD")
-
     conn_str = (
         "DRIVER={ODBC Driver 18 for SQL Server};"
-        f"SERVER={server};"
-        f"DATABASE={database};"
-        f"UID={username};"
-        f"PWD={password};"
+        "SERVER=223.177.191.70,1433;"
+        "DATABASE=posbhaji;"
+        "UID=sa;"
+        "PWD=485526365;"
         "Encrypt=no;"
         "TrustServerCertificate=yes;"
+        "Connection Timeout=30;"
+        "Network Library=DBMSSOCN;"
     )
 
     return pyodbc.connect(conn_str)
