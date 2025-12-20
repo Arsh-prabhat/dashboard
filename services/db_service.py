5396c9c27a28988c5eeb16d975c266387aa4e441
Arsh-prabhat <prabhatarsh1974@gmail.com>
2025-12-20 12:44:37 +0530

    Update ODBC driver and encryption settings

--- services/db_service.py
+++ services/db_service.py
@@ -2,12 +2,12 @@ import pyodbc
 
 def get_db_connection():
     conn_str = (
-        "DRIVER={ODBC Driver 17 for SQL Server};"
+        "DRIVER={ODBC Driver 18 for SQL Server};"
         "SERVER=223.177.191.70,1433;"
         "DATABASE=posbhaji;"
         "UID=sa;"
         "PWD=485526365;"
-        "Encrypt=yes;"
+        "Encrypt=no;"
         "TrustServerCertificate=yes;"
         "Connection Timeout=30;"
     )
